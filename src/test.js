18c708d0accf6db7d09307e24d42bd202017348b
Ian Collins <ian.michael.collins@gmail.com>
2020-10-09 23:23:47 +1100

    Base gears functions including getGearInfo

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 const gears = require('./utils/gears.js')
 
-const test = gears.gearRatio (49, 15)
+const test = gears.getGearInfo(50, 11, 25, '700c')
 
 console.log(test)
 
--- src/utils/gears.js
+++ src/utils/gears.js
@@ -1,7 +1,65 @@
-const gearRatio = (chainRing, cog) => {
+const getGearRatio = (chainRing, cog) => {
     return chainRing / cog
 }
 
+const getGearInches = (gearRatio) => {
+    return gearRatio * 27
+}
+
+const getWheelCircumfrance = (rimDiameter, tyreWidth) => {
+    return (rimDiameter + (2 * tyreWidth)) * Math.PI
+}
+
+const getRollOut = (gearRatio, wheelCircumfrance) => {
+    return gearRatio * wheelCircumfrance
+}
+
+const rimOptions = [
+    {
+        type: '700c',
+        size: 622,
+        description: '700c (622mm)'
+    },
+    {
+        type: '650c',
+        size: 571,
+        description: '650c (571mm)'
+    }
+]
+
+const getRimSizeByType = (type) => {
+    const rim = rimOptions.find((rim) => rim.type === type)
+    if (rim) {
+        return rim.size
+    } else {
+        return undefined
+    }   
+}
+
+const getGearInfo = (chainRing, cog, tyreWidth = 23, rimType = '700c') => {
+    const gearRatio = getGearRatio(chainRing, cog)
+    const gearInches = getGearInches(gearRatio)
+    const rimDiameter = getRimSizeByType(rimType)
+    const wheelCircumfrance = getWheelCircumfrance(rimDiameter, tyreWidth)
+    const rollOut = getRollOut(gearRatio, wheelCircumfrance)
+    return {
+        chainRing,
+        cog,
+        tyreWidth,
+        rimType,
+        rimDiameter,
+        gearRatio,
+        gearInches,
+        wheelCircumfrance,
+        rollOut
+    }
+}
+
 module.exports = {
-    gearRatio: gearRatio
+    getGearRatio: getGearRatio,
+    getWheelCircumfrance: getWheelCircumfrance,
+    getGearInches: getGearInches,
+    getRollOut: getRollOut,
+    getRimSizeByType: getRimSizeByType,
+    getGearInfo: getGearInfo
 }
